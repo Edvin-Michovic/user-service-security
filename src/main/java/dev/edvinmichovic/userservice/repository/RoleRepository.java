164466eb11299b6ba464607f7daa8403abc68c05
Edvin-Michovic <edvinmichovic@gmail.com>
2023-11-07 02:37:40 +0200

    Added @Repository to the RoleRepository

--- src/main/java/dev/edvinmichovic/userservice/repository/RoleRepository.java
+++ src/main/java/dev/edvinmichovic/userservice/repository/RoleRepository.java
@@ -2,7 +2,9 @@ package dev.edvinmichovic.userservice.repository;
 
 import dev.edvinmichovic.userservice.model.Role;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface RoleRepository extends JpaRepository<Role, Long> {
     Role findByName(String name);
 }
