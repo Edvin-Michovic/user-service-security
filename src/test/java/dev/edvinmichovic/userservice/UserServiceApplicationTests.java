1977f4b55a941d0ade10bd709481b3e7535232c8
Edvin-Michovic <edvinmichovic@gmail.com>
2023-11-07 02:04:26 +0200

    Initial commit

---
+++ src/test/java/dev/edvinmichovic/userservice/UserServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.edvinmichovic.userservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
